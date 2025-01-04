12970ec10fbf2c7a43bf6c85acfb0f8f20203927
Malyshka <lesyamalyshko@gmail.com>
2025-01-04 21:30:33 +0300

    feat: resolve createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let result = [];
+    members.map((name) =>
+      typeof name === "string"
+        ? result.push(name.trim().substring(0, 1).toUpperCase())
+        : null
+    );
+    result.sort();
+    return String(result).replaceAll(",", "");
+  }
+  return false;
 }
 
 module.exports = {
