95663d8c5606b5e684f3bd764586342ba069d686
Malyshka <lesyamalyshko@gmail.com>
2025-01-05 19:08:23 +0300

    feat: resolve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const addition = options?.addition !== undefined ? String(options.addition) : '';
+  const additionSeparator = options?.additionSeparator || '|';
+  const repeatTimes = options?.repeatTimes || 1;
+  const additionRepeatTimes = options?.additionRepeatTimes || 1;
+  const separator = options?.separator || '+';
+
+  const addPart = str + (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+
+  return addPart + (separator + addPart).repeat(repeatTimes - 1);
 }
 
 module.exports = {
