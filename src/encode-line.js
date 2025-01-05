6c6134c8eb147fd544438dabe953385852559eac
Malyshka <lesyamalyshko@gmail.com>
2025-01-05 17:38:15 +0300

    feat: resolve encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = str.split('');
+  let prevSimbol = '';
+  let result = '';
+  arr.map((simb) => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] === simb && arr[i] === arr[i+1] ? count++ : null;
+    }
+    count > 0 ? result += prevSimbol == simb ? '' : `${count + 1}${simb}` : result += `${simb}`;
+    prevSimbol = simb;
+
+  })
+  return result;
 }
 
 module.exports = {
